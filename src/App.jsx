127b56d6b8039935c34b302d09adbd174717c928
Abdullah Al Nirob <abdullahalnirob12@gmail.com>
2024-10-13 23:48:32 +0600

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <center>
+        <h1 className="mt-[100px] text-xl">Let's Craft Our Carrer</h1>
+      </center>
+    </div>
+  );
+};
+
+export default App;
