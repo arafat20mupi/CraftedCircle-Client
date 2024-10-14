f5df71d68abd6f4171deaa3f084ab68e148e3e66
snh-sabbir <me.snhsabbir@gmail.com>
2024-10-14 20:42:08 +0600

    first commit

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./footer/Footer";
 
 const Home = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer>
+
+            </Footer>
         </div>
     );
 };
