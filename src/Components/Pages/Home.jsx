037bb8b642720f8c366c90217191b9ba8a60ea63
tanjimN4 <tanjimahemed7@gmail.com>
2024-10-14 22:42:14 +0600

    navbar add

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./footer/Footer";
+import NavBar from "../Share/NavBar";
 
 const Home = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer>
 
